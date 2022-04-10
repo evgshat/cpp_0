13a7f0a934d90d47a64ae5bd58d42af7129caa4e
Laucher Charlette <lcharlet@ga-b1.msk.21-school.ru>
2022-04-10 14:20:40 +0300

    cpp00 ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,31 +1,20 @@
 #include <iostream>
 #include <cctype>
 
+
+
 int	main(int argc, char **argv)
 {
-	int	i;
-	int	len;
-	int	j;
-
-	i = 1;
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
-		while (i < argc)
+		for (int i = 1; i < argc; ++i)
 		{
-			j = 0;
-			len = strlen(argv[i]);
-			while (j < len)
-			{
-				if (j == len - 1)
-					std::cout << (char)toupper(argv[i][j]) << std::endl;
-				else
-					std::cout << (char)toupper(argv[i][j]);
-				j++;
-			}
-			i++;
+			for (size_t j = 0; j < strlen(argv[i]); ++j)
+				std::cout << static_cast<char>(toupper(argv[i][j]));
 		}
+		std::cout << '\n';
 	}
 	return (0);
 }
